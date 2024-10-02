235da52aaaa91a0dc23c3f908aaa5241e4cc260f
generaloss <goloborodkojava@gmail.com>
2024-10-02 12:43:49 +0300

    added Insets

---
+++ src/main/java/jpize/util/Insets.java
@@ -0,0 +1,58 @@
+package jpize.util;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Insets implements Serializable {
+
+    public float top;
+    public float left;
+    public float bottom;
+    public float right;
+
+    public Insets(int top, int left, int bottom, int right) {
+        this.set(top, left, bottom, right);
+    }
+
+    public Insets(Insets insets) {
+        this.set(insets);
+    }
+
+
+    public void set(float top, float left, float bottom, float right) {
+        this.top = top;
+        this.left = left;
+        this.bottom = bottom;
+        this.right = right;
+    }
+
+    public void set(Insets insets) {
+        this.set(insets.top, insets.left, insets.bottom, insets.right);
+    }
+
+
+    public Insets copy() {
+        return new Insets(this);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if(this == object)
+            return true;
+        if(object == null || getClass() != object.getClass())
+            return false;
+        final Insets insets = (Insets) object;
+        return Float.compare(top, insets.top) == 0 && Float.compare(left, insets.left) == 0 && Float.compare(bottom, insets.bottom) == 0 && Float.compare(right, insets.right) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(top, left, bottom, right);
+    }
+
+    @Override
+    public String toString() {
+        return top + ", " + left + ", " + bottom + ", " + right;
+    }
+
+}
