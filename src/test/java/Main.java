ae74671bb0e0d8b71de27fbfb7501d567d45a933
generaloss <goloborodkojava@gmail.com>
2025-01-16 21:55:41 +0300

    account test commit

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         final FastReader reader = new FastReader();
+
         while(reader.hasNext()){
             System.out.println(reader.next());
         }
