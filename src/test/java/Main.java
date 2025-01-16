970daa2814aa7c7931b4b02fc1edc73448279d74
generaloss <goloborodkojava@gmail.com>
2025-01-16 22:22:00 +0300

    account test commit #2

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,13 +1,8 @@
-import jpize.util.io.FastReader;
 
 public class Main {
 
     public static void main(String[] args) {
-        final FastReader reader = new FastReader();
 
-        while(reader.hasNext()){
-            System.out.println(reader.next());
-        }
     }
 
 }
