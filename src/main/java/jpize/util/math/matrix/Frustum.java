19aaadb932d9dfbe96e97ac416431cf9c8ab54d9
generaloss <goloborodkojava@gmail.com>
2025-01-22 07:28:28 +0300

    improve Frustum

--- src/main/java/jpize/util/math/matrix/Frustum.java
+++ src/main/java/jpize/util/math/matrix/Frustum.java
@@ -1,106 +1,90 @@
 package jpize.util.math.matrix;
 
-import jpize.util.math.Mathc;
+import jpize.util.math.axisaligned.AABoxBody;
+import jpize.util.math.vector.Vec3f;
 import jpize.util.math.vector.Vec4f;
 
 import static jpize.util.math.matrix.Matrix4.*;
 
 public class Frustum {
 
-    private final Vec4f[] val;
+    private final Vec4f[] vectors;
 
     public Frustum() {
-        this.val = new Vec4f[6];
-        for(int i = 0; i < val.length; i++)
-            val[i] = new Vec4f();
+        this.vectors = new Vec4f[]{
+            new Vec4f(), new Vec4f(), new Vec4f(),
+            new Vec4f(), new Vec4f(), new Vec4f()
+        };
     }
 
-    public Frustum(Matrix4f view, Matrix4f proj) {
+    public Frustum(Matrix4f combined) {
         this();
-        this.setFrustum(proj, view);
+        this.setFrustum(combined);
     }
 
-    public Frustum(Matrix4f comb) {
+    public Frustum(Matrix4f view, Matrix4f projection) {
         this();
-        this.setFrustum(comb);
+        this.setFrustum(projection, view);
     }
 
 
-    public void setFrustum(Matrix4f view, Matrix4f proj) {
-        final float[] comb = Matrix4f.mul(proj.val, view.val);
-        this.setFrustum(comb);
+    public void setFrustum(float[] combined) {
+        final float v0 = combined[m03];
+        final float v1 = combined[m13];
+        final float v2 = combined[m23];
+        final float v3 = combined[m33];
+
+        vectors[0].set((v0 - combined[m00]), (v1 - combined[m10]), (v2 - combined[m20]), (v3 - combined[m30])).nor();
+        vectors[1].set((v0 + combined[m00]), (v1 + combined[m10]), (v2 + combined[m20]), (v3 + combined[m30])).nor();
+        vectors[2].set((v0 + combined[m01]), (v1 + combined[m11]), (v2 + combined[m21]), (v3 + combined[m31])).nor();
+        vectors[3].set((v0 - combined[m01]), (v1 - combined[m11]), (v2 - combined[m21]), (v3 - combined[m31])).nor();
+        vectors[4].set((v0 - combined[m02]), (v1 - combined[m12]), (v2 - combined[m22]), (v3 - combined[m32])).nor();
+        vectors[5].set((v0 + combined[m02]), (v1 + combined[m12]), (v2 + combined[m22]), (v3 + combined[m32])).nor();
     }
 
     public void setFrustum(Matrix4f combined) {
         this.setFrustum(combined.val);
     }
 
-    public void setFrustum(float[] combined) {
-        val[0].x = (combined[m03] - combined[m00]);
-        val[0].y = (combined[m13] - combined[m10]);
-        val[0].z = (combined[m23] - combined[m20]);
-        val[0].w = (combined[m33] - combined[m30]);
-
-        val[1].x = (combined[m03] + combined[m00]);
-        val[1].y = (combined[m13] + combined[m10]);
-        val[1].z = (combined[m23] + combined[m20]);
-        val[1].w = (combined[m33] + combined[m30]);
-
-        val[2].x = (combined[m03] + combined[m01]);
-        val[2].y = (combined[m13] + combined[m11]);
-        val[2].z = (combined[m23] + combined[m21]);
-        val[2].w = (combined[m33] + combined[m31]);
-
-        val[3].x = (combined[m03] - combined[m01]);
-        val[3].y = (combined[m13] - combined[m11]);
-        val[3].z = (combined[m23] - combined[m21]);
-        val[3].w = (combined[m33] - combined[m31]);
-
-        val[4].x = (combined[m03] - combined[m02]);
-        val[4].y = (combined[m13] - combined[m12]);
-        val[4].z = (combined[m23] - combined[m22]);
-        val[4].w = (combined[m33] - combined[m32]);
-
-        val[5].x = (combined[m03] + combined[m02]);
-        val[5].y = (combined[m13] + combined[m12]);
-        val[5].z = (combined[m23] + combined[m22]);
-        val[5].w = (combined[m33] + combined[m32]);
-
-        for(int i = 0; i < 6; i++)
-            this.divide(i);
+    public void setFrustum(Matrix4f view, Matrix4f projection) {
+        final float[] combined = Matrix4f.mul(projection.val, view.val);
+        this.setFrustum(combined);
     }
 
-    private void divide(int index) {
-        final float sqrt = Mathc.sqrt(
-            val[index].x * val[index].x +
-            val[index].y * val[index].y +
-            val[index].z * val[index].z
-        );
-
-        val[index].x /= sqrt;
-        val[index].y /= sqrt;
-        val[index].z /= sqrt;
-        val[index].w /= sqrt;
-    }
 
     private float multiply(int index, float x, float y, float z) {
-        return val[index].x * x  +  val[index].y * y  +  val[index].z * z  +  val[index].w;
+        final Vec4f vector = vectors[index];
+        return (vector.x * x) + (vector.y * y) + (vector.z * z) + vector.w;
     }
 
-    public boolean isBoxInFrustum(float x1, float y1, float z1, float x2, float y2, float z2) {
-        for(int i = 0; i < 6; i++)
-            if(this.multiply(i, x1, y1, z1) <= 0F && this.multiply(i, x2, y1, z1) <= 0F && this.multiply(i, x1, y2, z1) <= 0F &&
-               this.multiply(i, x2, y2, z1) <= 0F && this.multiply(i, x1, y1, z2) <= 0F && this.multiply(i, x2, y1, z2) <= 0F &&
-               this.multiply(i, x1, y2, z2) <= 0F && this.multiply(i, x2, y2, z2) <= 0F)
-                return false;
-        return true;
-    }
 
-    public boolean isVertexInFrustum(float x, float y, float z) {
+    public boolean isPointIn(float x, float y, float z) {
         for(int i = 0; i < 6; i++)
             if(this.multiply(i, x, y, z) <= 0F)
                 return false;
         return true;
+    }
+
+    public boolean isPointIn(Vec3f point) {
+        return this.isPointIn(point.x, point.y, point.z);
+    }
+
+
+    public boolean isAABoxIn(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
+        for(int i = 0; i < 6; i++)
+            if(this.multiply(i, minX, minY, minZ) <= 0F && this.multiply(i, maxX, minY, minZ) <= 0F && this.multiply(i, minX, maxY, minZ) <= 0F &&
+               this.multiply(i, maxX, maxY, minZ) <= 0F && this.multiply(i, minX, minY, maxZ) <= 0F && this.multiply(i, maxX, minY, maxZ) <= 0F &&
+               this.multiply(i, minX, maxY, maxZ) <= 0F && this.multiply(i, maxX, maxY, maxZ) <= 0F)
+                return false;
+        return true;
+    }
+
+    public boolean isAABoxIn(Vec3f min, Vec3f max) {
+        return this.isAABoxIn(min.x, min.y, min.z, max.x, max.y, max.z);
+    }
+
+    public boolean isAABoxIn(AABoxBody box) {
+        return this.isAABoxIn(box.getMin(), box.getMax());
     }
 
 }
