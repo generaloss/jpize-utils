1bfc3edeb45b4b1e1be91172d160ba3feaa9ce99
generaloss <generaloss@proton.me>
2025-03-30 13:04:25 +0300

    improve BitMask

--- src/main/java/jpize/util/BitMask.java
+++ src/main/java/jpize/util/BitMask.java
@@ -10,8 +10,18 @@ public class BitMask {
         this.bits = bits;
     }
 
+
     public int getBits() {
         return bits;
+    }
+
+    public void setBits(int bits) {
+        this.bits = bits;
+    }
+
+
+    public boolean isDefault() {
+        return (bits == 0);
     }
 
 
